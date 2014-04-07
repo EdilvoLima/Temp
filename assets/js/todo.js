42a770e59569b4622130182a50a330369888fd6d
PedroVictorB <pedrovictor_caldas@hotmail.com>
2014-04-07 11:03:05 -0300

    Prioridade
    Links para subir ou descer a prioridade foram colocados.

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -30,5 +30,24 @@ function TodoCtrl($scope) {
     $scope.todos.splice(start, 1);
   }
   
+  $scope.move = function(index, direction){
+    
+    if (direction === 'up') {
+      if (index === 0) {
+        return;
+      }
+      index = index - 1;
+    }
+    
+    if (direction === 'down') {
+      if (index === $scope.todos.lenght - 1) {
+        return;
+      }
+    }
+    
+    var todo = $scope.todos[index];
+    $scope.todos.splice(index + 2, 0, todo);
+    $scope.todos.splice(index, 1);
+  }
   
 };
