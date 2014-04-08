cac725edcd90568471968aa213a33b3a16b2fa90
PedroVictorB <pedrovictor_caldas@hotmail.com>
2014-04-08 14:27:02 -0300

    Persistência
    Modelo de persistência.

--- api/models/Todo.js
+++ api/models/Todo.js
@@ -22,7 +22,7 @@ var Todo = {
             },
             done:{
                   type: 'BOOLEAN',
-                  required: 'true'
+                  required: true
             }
       }
 };
